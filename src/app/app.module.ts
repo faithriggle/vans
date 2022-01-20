02e48ea3d61e6a94160707982dc46bf409cd7345
Faith <fer00001@mix.wvu.edu>
2022-01-19 23:42:06 -0500

    added top navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { topNavBarComponent } from 'src/navigation/topnavbar.component';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    topNavBarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/navigation/topnavbar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'vans-topnavbar',
+    templateUrl: 'topnavbar.component.html'
+})
+export class topNavBarComponent {
+
+}
