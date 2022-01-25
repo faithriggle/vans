13a782a10e49507d06277b714168a1dd3eb263e3
Faith <fer00001@mix.wvu.edu>
2022-01-24 20:53:51 -0500

    added css files for nav bars

--- src/navigation/bottomnavbar.component.ts
+++ src/navigation/bottomnavbar.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component ({
     selector: 'vans-bottomnavbar',
-    templateUrl: 'bottomnavbar.component.html'
+    templateUrl: 'bottomnavbar.component.html',
+    styleUrls: ['bottomnavbar.component.css']
 })
 export class bottomNavBarComponent {
 
--- src/navigation/topnavbar.component.ts
+++ src/navigation/topnavbar.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'vans-topnavbar',
-    templateUrl: 'topnavbar.component.html'
+    templateUrl: 'topnavbar.component.html',
+    styleUrls: ['topnavbar.component.css']
 })
 export class topNavBarComponent {
 
